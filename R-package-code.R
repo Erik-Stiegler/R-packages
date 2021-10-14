f32b926d377d2b01be12c46f3dde3067f6f2edca
Erik Stiegler <92512827+Erik-Stiegler@users.noreply.github.com>
2021-10-14 14:21:14 +0200

    Code for installning all packages to new Rstudio

---
+++ R-package-code.R
@@ -0,0 +1,120 @@
+# installing/loading the package:
+if(!require(RMySQL)) {
+  install.packages("RMySQL"); 
+  require(RMySQL)
+} #load / install+load RMySQL
+
+# installing/loading the package:
+if(!require(dplyr)) {
+  install.packages("dplyr"); 
+  require(dplyr)
+} #load / install+load dplyr
+
+# installing/loading the package:
+if(!require(tidyr)) {
+  install.packages("tidyr"); 
+  require(tidyr)
+} #load / install+load tidyr
+
+# installing/loading the package:
+if(!require(stringr)) {
+  install.packages("stringr"); 
+  require(stringr)
+} #load / install+load stringr
+
+
+# installing/loading the package:
+if(!require(lubridate)) {
+  install.packages("lubridate"); 
+  require(lubridate)
+} #load / install+load lubridate
+
+# installing/loading the package:
+if(!require(ggplot2)) {
+  install.packages("ggplot2"); 
+  require(ggplot2)
+} #load / install+load ggplot2
+
+# installing/loading the package:
+if(!require(ggthemes)) {
+  install.packages("ggthemes"); 
+  require(ggthemes)
+} #load / install+load ggthemes
+
+# installing/loading the package:
+if(!require(shiny)) {
+  install.packages("shiny"); 
+  require(shiny)
+} #load / install+load shiny
+
+# installing/loading the package:
+if(!require(rmarkdown)) {
+  install.packages("rmarkdown"); 
+  require(rmarkdown)
+} #load / install+load rmarkdown
+
+# installing/loading the package:
+if(!require(xtable)) {
+  install.packages("xtable"); 
+  require(xtable)
+} #load / install+load xtable
+
+# installing/loading the package:
+if(!require(downloader)) {
+  install.packages("downloader"); 
+  require(downloader)
+} #load / install+load downloader
+
+# installing/loading the package:
+if(!require(repmis)) {
+  install.packages("repmis"); 
+  require(repmis)
+} #load / install+load repmis
+
+# installing/loading the package:
+if(!require(readxl)) {
+  install.packages("readxl"); 
+  require(readxl)
+} #load / install+load readxl
+
+# installing/loading the package:
+if(!require(arules)) {
+  install.packages("arules"); 
+  require(arules)
+} #load / install+load arules
+
+# installing/loading the package:
+if(!require(arulesViz)) {
+  install.packages("arulesViz"); 
+  require(arulesViz)
+} #load / install+load arulesViz
+
+# installing/loading the package:
+if(!require(neuralnet)) {
+  install.packages("neuralnet"); 
+  require(neuralnet)
+} #load / install+load neuralnet
+
+# installing/loading the package:
+if(!require(factoextra)) {
+  install.packages("factoextra"); 
+  require(factoextra)
+} #load / install+load factoextra
+
+# installing/loading the package:
+if(!require(aod)) {
+  install.packages("aod"); 
+  require(aod)
+} #load / install+load aod
+
+# installing/loading the package:
+if(!require(grid)) {
+  install.packages("grid"); 
+  require(grid)
+} #load / install+load grid
+
+# installing/loading the package:
+if(!require(Matrix)) {
+  install.packages("Matrix"); 
+  require(Matrix)
+} #load / install+load Matrix
